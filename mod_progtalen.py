af56446decef62c108017474a1807dca2a3b83ee
adriaandb <adriaan.debelder@gmail.com>
2022-01-29 17:08:10 +0100

    module programmeurs

---
+++ mod_progtalen.py
@@ -0,0 +1,12 @@
+'''
+talenlijst=[
+        {"naam":"Andreas" , "talen" : ["Python" , "Java" , "Html"]},
+        {"naam": "Johan" , "talen" : ["Python" , "Java" , "Scratch" , "php" , "OpenJS"]},
+        {"naam" : "Jos" , "talen" : ["html" , "css" , "C+"]}
+            ]
+ '''
+
+talenlijst={ "Andreas" : ["Python" , "Java" , "Html"],
+        "Johan" : ["Python" , "Java" , "Scratch" , "php" , "OpenJS"],
+        "Jos" : ["html" , "css" , "C+"]
+        }
